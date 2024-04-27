b9f6b2765f87fc75217e6868a6b99c52cab4b2c7
Spencer <spencerfbrandon@gmail.com>
2024-04-27 23:44:42 +0000

    Test Numbers

--- CalculatorJS.js
+++ CalculatorJS.js
@@ -2,6 +2,17 @@ let firstNumber = [];
 let operation = null;
 let secondNumber = [];
 
-function setFirstNumber() {
-    console.log("HelloWorld");
+function NumberTest(Number) {
+    firstNumber.push(Number);
+    console.log(firstNumber);
 }
+
+document.addEventListener ('DOMContentLoaded', function() {
+    const Numbers = document.querySelectorAll('.CalculatorButton')
+    Numbers.forEach(function(button) {
+        button.addEventListener('click', function() {
+            let Number = parseInt(this.textContent);
+            NumberTest(Number);
+        })
+    })
+});
